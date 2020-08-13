ce2b3f174a6e29c45b860c3a134226be6b799044
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-08-13 13:23:45 -0400

    added games web service

--- app/src/main/java/com/maxor/digitalturbine/games/GamesService.kt
+++ app/src/main/java/com/maxor/digitalturbine/games/GamesService.kt
@@ -1,5 +1,26 @@
 package com.maxor.digitalturbine.games
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+
 interface GamesService {
+
+    companion object {
+        val baseUrl: String = "http://ads.appia.com/"
+
+        fun create(): GamesService {
+            val httpClient = OkHttpClient.Builder()
+
+            return  Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+
+                .client(httpClient.build())
+                .build().create(GamesService::class.java)
+        }
+    }
 
 }
