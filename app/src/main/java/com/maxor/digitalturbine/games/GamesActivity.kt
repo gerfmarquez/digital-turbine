3ad88a0b323ecd8d298b6b0725f21d035dae0407
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-08-13 13:14:26 -0400

    created activity that will contain game ads

---
+++ app/src/main/java/com/maxor/digitalturbine/games/GamesActivity.kt
@@ -0,0 +1,13 @@
+package com.maxor.digitalturbine.games
+
+import android.app.Activity
+import android.os.Bundle
+
+class GamesActivity : Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
